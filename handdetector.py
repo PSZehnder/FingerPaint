43d3d29fd6192c3814ce1ee3c0a2c21abf443cad
Philip Zehnder <philipszehnder@gmail.com>
2020-04-09 00:56:34 -0700

    added example gif

--- handdetector.py
+++ handdetector.py
@@ -1,7 +1,7 @@
 from webcam import WebcamYolo
 import cv2
 
-webyolo = WebcamYolo(config='./models/yolo/yolov3-custom.cfg', weights='./models/yolo/yolov3_ckpt_34.pth',
+webyolo = WebcamYolo(config='./models/yolo/yolov3-custom.cfg', weights='./models/yolo/yolov3_ckpt_49.pth',
                      fps=60, class_labels='./models/yolo/classes.names', confscore=0.5)
 print(webyolo.device)
 webyolo.run()
